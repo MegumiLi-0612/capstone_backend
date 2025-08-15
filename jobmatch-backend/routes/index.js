ecfe1b83c79043049c98eae4f44b3fe764ea3def
Megumi <huihaoli2025@u.northwestern.edu>
2025-08-15 15:25:46 -0500

    Update index.js

--- jobmatch-backend/routes/index.js
+++ jobmatch-backend/routes/index.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const authRoutes = require('./auth.routes');
+const userRoutes = require('./user.routes');
 
 router.get('/ping', (req, res) => res.json({ pong: true }));
-router.use('/auth', authRoutes);  // => /api/v1/auth/...
+
+router.use('/auth', authRoutes);     // => /api/v1/auth/...
+router.use('/users', userRoutes);    // => /api/v1/users/...
 
 module.exports = router;
